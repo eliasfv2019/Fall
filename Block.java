c51b9e0b8a41d97437b97aa710b1e177efb84f2e
eliasfv2019 <32773105+eliasfv2019@users.noreply.github.com>
2019-02-04 11:45:05 -0400

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,62 @@
+import greenfoot.*;
+import java.util.List;
+/**
+ * A block that bounces back and forth across the screen.
+ * 
+ * @author Michael Kölling
+ * @version 0.1
+ */
+public class Block extends Actor
+{
+    private int delta = 2;
+
+    /**
+     * Move across the screen, bounce off edges. Turn leaves, if we touch any.
+     */
+    public void act() 
+    {
+        move();
+        checkEdge();
+        checkMouseClick();
+        Leaf leaf = (Leaf) getOneIntersectingObject(Leaf.class);
+                if(leaf!=null){
+           leaf.turn(3);
+        }
+    }
+
+    /**
+     * Move sideways, either left or right.
+     */
+    private void move()
+    {
+        setLocation(getX()+delta, getY());
+    }
+
+    /**
+     * Check whether we are at the edge of the screen. If we are, turn around.
+     */
+    private void checkEdge()
+    {
+        if (isAtEdge()) 
+        {
+            delta = -delta;
+            World world = getWorld();
+            world.addObject(new Leaf(),getX(),getY());
+            // reverse direction
+        }
+    }
+
+    /**
+     * Check whether the mouse button was clicked. If it was, change all leaves.
+     */
+    private void checkMouseClick()
+    {
+        if (Greenfoot.mouseClicked(null)) 
+        {
+           List<Leaf>getWorld;
+            
+            // do this when the mouse is clicked. currently: nothing.
+        }
+    }
+
+}
